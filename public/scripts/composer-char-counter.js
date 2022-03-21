aa68f80da1caf1abdbe9e50bb400fa793a67bac4
Ziyu Huang <aprilhuang825@gmail.com>
2022-03-21 22:12:38 +0000

    counter turn red when exceed 140 character limit

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,7 +3,12 @@ $(document).ready(function() {
   $('.new-tweet textarea').on('input', function() {
     const tweetLength = $(this).val().length;
     const newCounter = $(this).siblings('.counter'); 
-    const tweetLimit = 140;
-    newCounter.text(tweetLimit-tweetLength);
+    const remainingLen = 140-tweetLength;
+    newCounter.text(remainingLen);
+    if(remainingLen < 0) {
+      newCounter.addClass('over-limit-color');
+    } else {
+      newCounter.removeClass('over-limit-color');
+    }
   }) 
 });
